4414c739566907ce14aea72c7dca5ac2ee48c75a
姚皓 <yaohao.yh@alibaba-inc.com>
2021-06-02 10:16:19 +0800

    代码重构

--- jplag.frontend.text/src/main/java/jplag/text/Language.java
+++ jplag.frontend.text/src/main/java/jplag/text/Language.java
@@ -7,76 +7,75 @@ import jplag.IProgram;
 
 /**
  * @Changed by Emeric Kwemou 29.01.2005
- *  
  */
 public class Language implements jplag.Language {
 
-	private IProgram program;
+    private IProgram program;
 
-	private jplag.text.Parser parser = new jplag.text.Parser();
+    private jplag.text.Parser parser = new jplag.text.Parser();
 
-	public Language(IProgram program) {
-		this.program = program;
-		this.parser.setProgram(this.program);
-	}
+    public Language(IProgram program) {
+        this.program = program;
+        this.parser.setProgram(this.program);
+    }
 
-	@Override
-	public int errorsCount() {
-		return this.parser.errorsCount();
-	}
+    @Override
+    public int errorsCount() {
+        return this.parser.errorsCount();
+    }
 
-	@Override
-	public String[] suffixes() {
-		String[] res = { ".TXT", ".txt", ".ASC", ".asc", ".TEX", ".tex" };
-		return res;
-	}
+    @Override
+    public String[] suffixes() {
+        String[] res = {".TXT", ".txt", ".ASC", ".asc", ".TEX", ".tex", ".js", ".jsx"};
+        return res;
+    }
 
-	@Override
-	public String name() {
-		return "Text Parser";
-	}
+    @Override
+    public String name() {
+        return "Text Parser";
+    }
 
-	@Override
-	public String getShortName() {
-		return "text";
-	}
+    @Override
+    public String getShortName() {
+        return "text";
+    }
 
-	@Override
-	public int min_token_match() {
-		return 5;
-	}
+    @Override
+    public int min_token_match() {
+        return 5;
+    }
 
-	public jplag.Structure parse(File dir, String[] files) {
-		return this.parser.parse(dir, files);
-	}
+    public jplag.Structure parse(File dir, String[] files) {
+        return this.parser.parse(dir, files);
+    }
 
-	@Override
-	public boolean errors() {
-		return this.parser.getErrors();
-	}
+    @Override
+    public boolean errors() {
+        return this.parser.getErrors();
+    }
 
-	@Override
-	public boolean supportsColumns() {
-		return true;
-	}
+    @Override
+    public boolean supportsColumns() {
+        return true;
+    }
 
-	@Override
-	public boolean isPreformated() {
-		return false;
-	}
+    @Override
+    public boolean isPreformated() {
+        return false;
+    }
 
-	@Override
-	public boolean usesIndex() {
-		return false;
-	}
+    @Override
+    public boolean usesIndex() {
+        return false;
+    }
 
-	@Override
-	public int noOfTokens() {
+    @Override
+    public int noOfTokens() {
         return parser.tokenStructure.serial;
 //		return jplag.text.TextToken.numberOfTokens();   // always returns 1 ....
-	}
+    }
 
-	public String type2string(int type) {
-		return jplag.text.TextToken.type2string(type);
-	}
+    public String type2string(int type) {
+        return jplag.text.TextToken.type2string(type);
+    }
 }
